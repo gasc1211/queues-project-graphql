109dfe2f7dae431586c363f2cbf16a160a42b295
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2024-12-05 18:36:15 -0600

    Add type definitions

---
+++ src/typedefs.ts
@@ -0,0 +1,46 @@
+export const typeDefs = `#graphql
+
+  type User{
+    user_id: ID!
+    first_name: String
+    last_name: String
+    email: String
+    posts: [Post]
+    roles: [Role] 
+  }
+  
+  type Role {
+    role_id: ID!
+    description: String 
+  }
+
+  type UserRole {
+    user: User
+    role: Role
+  }
+
+  type Post {
+    post_id: ID!
+    content: String
+    author: User
+    comments: [Comment]
+  }
+  
+  type Comment {
+    comment_id: ID!
+    content: String
+    author: User
+    post: Post
+  }
+
+  type Query {
+    Users(user_id: Int): [User]
+    Posts(post_id: Int): [Post]
+    Roles(role_id: Int): [Role]
+    Comments(comment_id: Int): [Comment]
+  }
+
+  # type UserPosts{}
+  # type UserRoles{}
+
+`;
